6a8589d4acacb98b3354537293ef068b8e458b36
David Lazarony <DRL@DavidLazarony.com>
2022-10-30 17:16:30 -0700

    Remove unnecessary console.log call.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side Javascript running!")
-
 // fetch("https://puzzle.mead.io/puzzle").then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
